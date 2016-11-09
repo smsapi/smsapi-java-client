c56bc9a2054e8233c34ffc27ed5f77675b6e05c0
Wacław Jacek <mail@waclawjacek.com>
2016-11-09 15:33:13 +0100

    Enkoder base64 z Apache Commons zamiast z Suna

--- src/main/java/pl/smsapi/api/authenticationStrategy/BasicAuthentication.java
+++ src/main/java/pl/smsapi/api/authenticationStrategy/BasicAuthentication.java
@@ -1,14 +1,13 @@
 package pl.smsapi.api.authenticationStrategy;
 
-import com.sun.org.apache.xml.internal.security.utils.Base64;
-
 import java.util.Map;
+import org.apache.commons.codec.binary.Base64;
 
 public class BasicAuthentication implements AuthenticationStrategy {
     @Override
     public String getAuthenticationHeader(Map<String, ?> data) {
         String authStr = data.get("username") + ":" + data.get("password");
-        authStr = Base64.encode(authStr.getBytes());
+        authStr = Base64.encodeBase64String(authStr.getBytes());
 
         return "Basic " + authStr;
     }
