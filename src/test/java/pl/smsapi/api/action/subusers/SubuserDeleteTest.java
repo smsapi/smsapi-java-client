89551613959f364cbcd83bfbaccb8dfee43a91b3
Maciej Lew <maciej.lew.1987@gmail.com>
2024-05-07 11:30:20 +0200

    Moves Subuser API unit tests to that API package

--- src/test/java/pl/smsapi/api/action/subusers/SubuserDeleteTest.java
+++ src/test/java/pl/smsapi/api/action/subusers/SubuserDeleteTest.java
@@ -1,7 +1,6 @@
-package pl.smsapi.test.unit.action.subusers;
+package pl.smsapi.api.action.subusers;
 
 import org.junit.Test;
-import pl.smsapi.api.action.subusers.SubuserGet;
 import pl.smsapi.exception.SmsapiException;
 import pl.smsapi.test.doubles.ClientStub;
 import pl.smsapi.test.doubles.ProxyRequestSpy;
@@ -10,18 +9,18 @@ import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 
-public class SubuserGetTest {
+public class SubuserDeleteTest {
 
     @Test
-    public void executeGetSubuserRequest() throws SmsapiException {
-        ProxyRequestSpy requestStub = new ProxyRequestSpy(SubuserResponseJsonMother.create());
-        SubuserGet action = new SubuserGet("0f0f0f0f0f0f0f0f0f0f0f0f");
+    public void executeDeleteSubuserRequest() throws SmsapiException {
+        ProxyRequestSpy requestStub = new ProxyRequestSpy(SubuserJsonMother.create());
+        SubuserDelete action = new SubuserDelete("0f0f0f0f0f0f0f0f0f0f0f0f");
         action.client(new ClientStub());
         action.proxy(requestStub);
 
         action.execute();
 
-        assertEquals("GET", requestStub.requestMethod);
+        assertEquals("DELETE", requestStub.requestMethod);
         assertEquals("subusers/0f0f0f0f0f0f0f0f0f0f0f0f", requestStub.requestEndpoint);
         HashMap<String, String> expectedRequestPayload = new HashMap<>();
         assertEquals(expectedRequestPayload, requestStub.requestPayload);
--- src/test/java/pl/smsapi/api/action/subusers/SubuserGetTest.java
+++ src/test/java/pl/smsapi/api/action/subusers/SubuserGetTest.java
@@ -1,7 +1,6 @@
-package pl.smsapi.test.unit.action.subusers;
+package pl.smsapi.api.action.subusers;
 
 import org.junit.Test;
-import pl.smsapi.api.action.subusers.SubuserDelete;
 import pl.smsapi.exception.SmsapiException;
 import pl.smsapi.test.doubles.ClientStub;
 import pl.smsapi.test.doubles.ProxyRequestSpy;
@@ -10,18 +9,18 @@ import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 
-public class SubuserDeleteTest {
+public class SubuserGetTest {
 
     @Test
-    public void executeDeleteSubuserRequest() throws SmsapiException {
-        ProxyRequestSpy requestStub = new ProxyRequestSpy(SubuserResponseJsonMother.create());
-        SubuserDelete action = new SubuserDelete("0f0f0f0f0f0f0f0f0f0f0f0f");
+    public void executeGetSubuserRequest() throws SmsapiException {
+        ProxyRequestSpy requestStub = new ProxyRequestSpy(SubuserJsonMother.create());
+        SubuserGet action = new SubuserGet("0f0f0f0f0f0f0f0f0f0f0f0f");
         action.client(new ClientStub());
         action.proxy(requestStub);
 
         action.execute();
 
-        assertEquals("DELETE", requestStub.requestMethod);
+        assertEquals("GET", requestStub.requestMethod);
         assertEquals("subusers/0f0f0f0f0f0f0f0f0f0f0f0f", requestStub.requestEndpoint);
         HashMap<String, String> expectedRequestPayload = new HashMap<>();
         assertEquals(expectedRequestPayload, requestStub.requestPayload);
