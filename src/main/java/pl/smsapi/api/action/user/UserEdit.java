066fe4f6c4fb42f501063de1bfd256347cf5902c
Maciej Lew <maciej.lew.1987@gmail.com>
2024-05-09 14:25:13 +0200

    Deprecates `pl.smsapi.api.UserFactory.actionAdd`, `pl.smsapi.api.UserFactory.actionEdit`, `pl.smsapi.api.UserFactory.actionList`

--- src/main/java/pl/smsapi/api/action/user/UserEdit.java
+++ src/main/java/pl/smsapi/api/action/user/UserEdit.java
@@ -1,5 +1,9 @@
 package pl.smsapi.api.action.user;
 
+/**
+ * @deprecated use {@link pl.smsapi.api.action.subusers.SubuserEdit()} instead
+ */
+@Deprecated
 public class UserEdit extends AbstractUserControl<UserEdit> {
 
     /**
