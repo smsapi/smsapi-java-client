ca9f2045ea0e7893066841cc78b4aa747cd951c5
Maciej Lew <maciej.lew.1987@gmail.com>
2024-05-16 07:44:11 +0200

    Adds Contacts API delete contact test

---
+++ src/test/java/pl/smsapi/api/action/contacts/ContactDeleteTest.java
@@ -0,0 +1,28 @@
+package pl.smsapi.api.action.contacts;
+
+import org.junit.Test;
+import pl.smsapi.exception.SmsapiException;
+import pl.smsapi.test.doubles.ClientStub;
+import pl.smsapi.test.doubles.ProxyRequestSpy;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class ContactDeleteTest {
+
+    @Test
+    public void executeDeleteSubuserRequest() throws SmsapiException {
+        ProxyRequestSpy requestStub = new ProxyRequestSpy("");
+        ContactDelete action = new ContactDelete("0f0f0f0f0f0f0f0f0f0f0f0f");
+        action.client(new ClientStub());
+        action.proxy(requestStub);
+
+        action.execute();
+
+        assertEquals("DELETE", requestStub.requestMethod);
+        assertEquals("contacts/0f0f0f0f0f0f0f0f0f0f0f0f", requestStub.requestEndpoint);
+        HashMap<String, String> expectedRequestPayload = new HashMap<>();
+        assertEquals(expectedRequestPayload, requestStub.requestPayload);
+    }
+}
